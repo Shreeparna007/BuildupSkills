7e524d20f0b4377c67a16d9808fddc8915fb6c32
TinkerBell <43819240+Shreeparna007@users.noreply.github.com>
2020-02-13 22:43:06 +0530

    Resize image implementation

---
+++ OpenCV/opencv.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread("galaxy.jpg", 1)
+print(img)
+print(type(img))
+print(img.shape)
+print(img.ndim)
+resized_image = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+cv2.imwrite("galaxy_resized.jpg", resized_image)
+cv2.imshow("galaxy", resized_image)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
